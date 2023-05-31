62609424662e326944df430d2fc5ebd79a775912
Andreas Stocker <shadowstep7@gmail.com>
2023-05-31 20:44:03 +0200

    add left panel, init tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex">
+    <div class="pl-20 pr-20 border-r-2 left-container">
+        <div class="pt-20 text-5xl name">
+            Andreas Stocker
+        </div>
+        <div class="pt-10 text-3xl">
+            Software Engineer
+        </div>
+        <div class="pt-6 text-xl">
+            Zurich, Switzerland
+        </div>
+        <div class="pt-6 text-xl underline">
+            <a href="https://github.com/stoand">github.com/stoand</a>
+        </div>
+        <div class="pt-4 text-xl underline">
+            <a href="mailto:stoand@protonmail.com">stoand@protonmail.com</a>
+        </div>
+    </div>
+    <div>
+        asdf
+    </div>
+</div>
+
+<style>
+    .name {
+        background: linear-gradient(to right, blue, purple);
+        background-clip: text;
+        color: transparent;
+        font-weight: bold;
+    }
+
+    .left-container {
+        height: 100vh;
+    }
+</style>
